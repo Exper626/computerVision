2434342cbe332d708d0f57e044d25b370ef28c01
Hanaa Azhar <azharhanaa2@gmail.com>
2025-05-07 14:30:46 +0530

    train_model.py and yolov8n.pt
    Model implementation done

---
+++ tomatoLeaf/train_model.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load a pre-trained YOLOv8 model
+model = YOLO('yolov8n.pt')
+
+# Train the model using the dataset
+model.train(
+    data='E:/computerVision/tomatoLeaf/data.yaml',
+    epochs=50,
+    imgsz=640,
+    batch=16
+)
